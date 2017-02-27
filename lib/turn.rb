20b5b74d2f9d319101e56b62b26f798e0616ebb2
Alex Segers <a.segers.97@gmail.com>
2017-02-27 01:12:15 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -46,6 +46,8 @@ def turn(board)
     until valid_move == true
       puts "Please enter 1-9:"
       input = gets.strip
+      index = input_to_index(input)
+      valid_move = valid_move?(board, index)
     end
     move(board, index, token = "X")
   end
